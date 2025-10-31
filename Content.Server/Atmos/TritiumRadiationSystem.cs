e7fb203af02416461b133f752a67f3654960a565
Carccborg <225926381+carccborg@users.noreply.github.com>
2025-10-31 07:12:44 -0400

    added TODO for clusterhelper

--- Content.Server/Atmos/TritiumRadiationSystem.cs
+++ Content.Server/Atmos/TritiumRadiationSystem.cs
@@ -13,13 +13,28 @@ namespace Content.Server.Atmos;
 [UpdateAfter(typeof(AtmosphereSystem))]
 public sealed class TritiumRadiationSystem : EntitySystem
 {
-	// TODO: implement
+	// TODO: implement main loop thing
 
+	// every 1-2s,
 	// for each grid,
-		// for each active tile on that grid,
-			// if the tile has no atmosphere, skip this tile
-			// get the number of moles of tritium on that tile
-			// if the number of moles is below a constant, skip this tile
-			// cluster tiles together and sum their sources together
-			// create a virtual radiation source every 1-2s using the clustered tiles' summed tritium mols
+	// for each active tile on that grid,
+	// if the tile has no atmosphere, skip this tile
+	// get the number of moles of tritium on that tile
+	// if the number of moles is below a constant, skip this tile
+	// cluster tiles together and sum their sources together
+	// create a virtual radiation source every 1-2s using the clustered tiles' summed tritium mols
+
+
+	// TODO: implement helper that clusters tritium tiles together
+
+	// take a list of coordinates
+	// for each coordinate
+	// if we've visited it, skip it
+	// if the tile has no atmosphere or the number of moles of tritium is below a constant, skip this tile
+	// create a new cluster and initialize its coordinates as the current coordinates
+	// merge neighbors (taken from the list of active tiles) within ClusterRadius
+	// increment the rads of the cluster by the rads of the merged neighbor
+	// mark the neighbor as visited
+	// repeat until there are no more unvisited neighbors
+	// return a list of clusters
 }
