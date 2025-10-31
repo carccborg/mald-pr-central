9251d60d555aa60802e28f6ad1fc5b73e106d098
Carccborg <225926381+carccborg@users.noreply.github.com>
2025-10-31 06:40:05 -0400

    radioactive_tritium: added TritiumRadiationSystem.cs
    Preliminary commit for filename and boilerplate stuff.
    
    Signed-off-by: Carccborg <225926381+carccborg@users.noreply.github.com>

---
+++ Content.Server/Atmos/TritiumRadiationSystem.cs
@@ -0,0 +1,15 @@
+using Content.Server.Atmos.EntitySystems;
+using Content.Server.Radiation.Systems;
+using Content.Shared.Atmos;
+using Robust.Shared.Map;
+using Robust.Shared.GameObjects;
+
+namespace Content.Server.Atmos;
+
+/// <summary>
+/// Applies radiation to nearby entities when Tritium exists in nearby active atmos tiles.
+/// <summary>
+public sealed class TritiumRadiationSystem : EntitySystem
+{
+	// TODO: implement
+}
